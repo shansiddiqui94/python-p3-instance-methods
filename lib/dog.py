693c3fda3deddeacb3c865ede1768126e8e4e818
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-03-18 21:10:13 -0400

    completed instance-methods

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,18 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    # This is an instance method:
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
+
+lassie = Dog()
+lassie.sit()
